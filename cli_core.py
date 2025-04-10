cc19c2bd1a1e75977e9c7549f81e9c687f3d9b76
makarkme <makarkme@mail.ru>
2025-04-10 19:37:37 +0300

    refactor cli, add change path to storage

--- cli_core.py
+++ cli_core.py
@@ -2,156 +2,132 @@
 # Чтоб не писать реализацию cli руками, можно использовать удобную альтернативу - typer
 
 import typer
-import json
-import ast
 import os
 import shutil
+import json
 
 from database import Database
 
-
 # Зачем MainCLI? - Впоследствии расширение функционала для работы с разными бд - пересохранение/копирование/переименование и тд.
-class MainCLI:
+class Storage:
     def __init__(self):
         pass
 
-    def list_databases(self, path: str = "./databases"):                        # Пример: python cli_core.py list
-        if not os.path.exists(path):                                            # Проверка, если путь не существует
-            typer.echo(f"[ERROR]: Directory '{path}' not found.")
+    def list_databases(self, path_to_storage: str = "./databases"):             # Пример: python cli_core.py list или python cli_core.py --storage-path "E:\PyCharmProjects\kbdSQL\storage" list
+
+        if not os.path.exists(path_to_storage):                                 # Проверка, если путь не существует
+            typer.echo(f"[ERROR]: Directory '{path_to_storage}' not found.")
             raise typer.Exit()
 
-        databases = os.listdir(path)
+        databases = os.listdir(path_to_storage)
         if not databases:
-            typer.echo(f"[WARNING]: No databases found in '{path}'.")
+            typer.echo(f"[WARNING]: Databases not found in '{path_to_storage}'.")
         else:
-            typer.echo(f"[DATABASES FIND IN]: '{path}'")
-            for name in databases:
-                typer.echo(f" -- {name}")
+            typer.echo(f"[DATABASES IN]: '{path_to_storage}'")
+            for database in databases:
+                typer.echo(f" -- {database}")
 
-    def create_database(self, name: str, path: str = "./databases"):            # Пример: python cli_core.py create mydb
-        full_path = os.path.join(path, name)
-        if not os.path.exists(full_path):                                       # Проверка, если путь не существует
-            os.makedirs(full_path)                                              # Создание директории
-            typer.echo(f"[CREATED]: '{name}' at '{path}'")
+    def create_database(self, database_name: str, path_to_storage: str = "./databases"):    # Пример: python cli_core.py create mydb или python cli_core.py --storage-path "E:\PyCharmProjects\kbdSQL\storage" create mydb
+        path_to_database = os.path.join(path_to_storage, database_name)
+        if not os.path.exists(path_to_database):                                # Проверка, если путь не существует
+            os.makedirs(path_to_database)                                       # Создание директории
+            typer.echo(f"[CREATED]: '{database_name}' at '{path_to_storage}'")
         else:
-            typer.echo(f"[WARNING]: Database '{name}' already exists.")
+            typer.echo(f"[WARNING]: Database '{database_name}' already exists.")
 
-    def delete_database(self, db_name: str, path: str = "./databases"):         # Пример: python cli_core.py delete mydb
-        full_path = os.path.join(path, db_name)
-        if not os.path.exists(full_path):                                       # Проверка, если путь не существует
-            typer.echo(f"[ERROR]: Database '{path}' not found.")
+    def delete_database(self, database_name: str, path_to_storage: str = "./databases"):    # Пример: python cli_core.py delete mydb или python cli_core.py --storage-path "E:\PyCharmProjects\kbdSQL\storage" delete mydb
+        path_to_database = os.path.join(path_to_storage, database_name)
+        if not os.path.exists(path_to_database):                                # Проверка, если путь не существует
+            typer.echo(f"[ERROR]: Database '{path_to_storage}' not found.")
             raise typer.Exit()
 
-        shutil.rmtree(full_path)                                                # Рекурсивно удаляет файл и всё её содержимое
-        typer.echo(f"[DELETED]: '{db_name}'")
+        shutil.rmtree(path_to_database)                                         # Рекурсивно удаляет файл и всё её содержимое
+        typer.echo(f"[DELETED]: '{database_name}'")
 
 
-# Зачем DBCLI? - Для работы с collections внутри бд. Впоследствии расширение функционала.
-class DBCLI:
-    def __init__(self, db_name: str, collection: str, path: str = "./databases"):
-        self.db = Database(db_name, path)
-        self.current_collection = self.db.get_collection(collection)
-        self.db_name = db_name
-        self.collection = collection
-
-
-    def insert(self, document: str):                                            # Пример: python cli_core.py db mydb/users insert "{'name': 'Иван', 'age': 18}"
-        if not self.current_collection:
-            typer.echo("[ERROR]: No collection selected! Use 'use' first.")
+class DB:
+    def __init__(self, current_database, path_to_storage, current_collection):
+        self.path_to_database = os.path.join(path_to_storage, current_database)
+        if not os.path.exists(self.path_to_database):                           # Проверка, если путь не существует
+            typer.echo(f"[ERROR]: Database '{current_database}' at '{path_to_storage}' not found.")
             raise typer.Exit()
 
+        self.database = Database(self.path_to_database, current_collection)
+
+    def insert(self, string):                                                   # Пример: python cli_core.py db mydb/users insert "{'name': 'Иван', 'age': 18}" или python cli_core.py --storage-path "E:\PyCharmProjects\kbdSQL\storage" db mydb/users insert "{'name': 'Иван', 'age': 18}"
         try:
-            doc = json.loads(document.replace("'", '"'))
-            doc_id = self.current_collection.collection_insert(doc)
-            typer.echo(f"[INSERT ID]: '{doc_id}'")
+            self.database.insert(string)
+            typer.echo("[INSERTED]: Successfully.")
+        except json.JSONDecodeError:
+            typer.echo("[ERROR]: Invalid JSON.")
         except Exception as e:
             typer.echo(f"[ERROR]: {e}")
 
-    def index(self, field: str):                                                # Пример: python cli_core.py db mydb/users index age
-        if not self.current_collection:
-            typer.echo("[ERROR]: No collection selected! Use 'use' first.")
-            raise typer.Exit()
-
-        count = self.current_collection.create_index(field)                     # Количество найденных значений
-        if count == 0:
-            typer.echo(f"[NOT FOUND]: '{field}'")
-        else:
-            typer.echo(f"[INDEX CREATE]: '{field}' ({count})")
-
-    def find(self, query: str):                                                 # Пример: python cli_core.py db mydb/users find "{'age': {'@eq': 18}}"
-        if not self.current_collection:
-            typer.echo("[ERROR]: No collection selected! Use 'use' first.")
-            raise typer.Exit()
-
+    def index(self, field):                                                     # Пример: python cli_core.py db mydb/users index age или python cli_core.py --storage-path "E:\PyCharmProjects\kbdSQL\storage" db mydb/users index age
         try:
-            try:
-                query_dict = json.loads(query)
-            except json.JSONDecodeError:
-                query_dict = ast.literal_eval(query)                            # Превращает "{'age': {'`$gt': 18}}" в словарь
-                print(query_dict)
-            results = self.current_collection.find_doc(query_dict)              # results - список "подходящих" документов
-            for json_doc in results:
-                typer.echo(json.dumps(json_doc, indent=2, ensure_ascii=False))  # json.dumps - превращает dict обратно в json; indent=2 - многострочное форматирование
-            typer.echo(f"[FOUND]: {len(results)} document(s).")
+            count = self.database.index(field)
+            typer.echo(f"[INDEXED]: {count} entries indexed by field '{field}'.")
         except Exception as e:
             typer.echo(f"[ERROR]: {e}")
 
-    # (нужно дописать реализацию)
-    # (удаляет документ по id)
-    def delete(self, doc_id: str):
-        pass
-
-    # # (удаляет документ по id)
-    # def delete(self, doc_id: str):
-    #     pass
-
-    # (вывожит все коллекции)
-    def list_collections(self):
-        pass
+    def search_by_condition(self, query):                                       # Пример: python cli_core.py db mydb/users condition "{'age': {'@eq': 18}}" или python cli_core.py --storage-path "E:\PyCharmProjects\kbdSQL\storage" db mydb/users condition "{'age': {'@eq': 18}}"
+        try:
+            json_documents = self.database.search_by_condition(query)           # Список "подходящих" json-документов
+            typer.echo("[FOUND]:")
+            for json_document in json_documents:
+                typer.echo(json_document)
+        except Exception as e:
+            typer.echo(f"[ERROR]: {e}")
 
 
 app = typer.Typer()
+db_app = typer.Typer()
 
 @app.callback()
-def main(ctx: typer.Context):
-    ctx.obj = MainCLI()
+def main(ctx: typer.Context, storage_path: str = typer.Option("./databases", "--storage-path", "-s", help="Путь к директории хранения баз данных.")):
+    ctx.obj = {"storage_path": storage_path}
 
 @app.command("list", help="Show databases.")
-def list_dbs(path: str = typer.Option("./databases")):
-    MainCLI().list_databases(path)
+def list_dbs(ctx: typer.Context):
+    storage_path = ctx.obj["storage_path"]
+    Storage().list_databases(storage_path)
 
 @app.command("create", help="Create database.")
-def create_db(name: str, path: str = typer.Option("./databases")):
-    MainCLI().create_database(name, path)
+def create_db(database_name: str, ctx: typer.Context):
+    storage_path = ctx.obj["storage_path"]
+    Storage().create_database(database_name, storage_path)
 
 @app.command("delete", help="Delete database.")
-def delete_db(name: str, path: str = typer.Option("./databases")):
-    MainCLI().delete_database(name, path)
-
-db_app = typer.Typer()
-app.add_typer(db_app, name="db")
+def delete_db(database_name: str, ctx: typer.Context):
+    storage_path = ctx.obj["storage_path"]
+    Storage().delete_database(database_name, storage_path)
 
 
 @db_app.callback()
-def db_callback(ctx: typer.Context, db_and_collection: str = typer.Argument(...), path: str = typer.Option("./databases")):
-    if "/" not in db_and_collection:
-        typer.echo("[ERROR]: Please use format 'name_database/collection'")
+def db_callback(ctx: typer.Context, database_and_collection: str = typer.Argument(..., help="Формат: 'database_name/current_collection'"), path_to_storage: str = typer.Option(None, help="Путь к директории хранения баз данных.")):
+    # Если параметр path_to_storage не указан явно для команды db, берем глобальное значение
+    if path_to_storage is None:
+        path_to_storage = ctx.obj["storage_path"]
+    if "/" not in database_and_collection:
+        typer.echo("[ERROR]: Please use format 'database_name/current_collection'")
         raise typer.Exit()
+    current_database, current_collection = database_and_collection.split("/", 1)
+    ctx.obj = DB(current_database, path_to_storage, current_collection)
 
-    db_name, collection = db_and_collection.split("/", 1)
-    ctx.obj = DBCLI(db_name, collection, path)
+@db_app.command("insert", help="Insert json-object in collection.")
+def insert(ctx: typer.Context, string: str):
+    ctx.obj.insert(string)
 
-@db_app.command("insert")
-def insert(ctx: typer.Context, document: str):
-    ctx.obj.insert(document)
-
-@db_app.command("index")
+@db_app.command("index", help="Index json-object in collection.")
 def index(ctx: typer.Context, field: str):
     ctx.obj.index(field)
 
-@db_app.command("find")
-def find(ctx: typer.Context, query: str):
-    ctx.obj.find(query)
+@db_app.command("condition", help="Search by condition in collection.")
+def search_by_condition(ctx: typer.Context, query: str):
+    ctx.obj.search_by_condition(query)
+
+
+app.add_typer(db_app, name="db")
 
 
 if __name__ == "__main__":
