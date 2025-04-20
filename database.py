ada4a2c8d3c7a598bafddcf7dc04a7d898a41d8c
makarkme <makarkme@mail.ru>
2025-04-21 02:01:54 +0300

    add new try-except + update comments

--- database.py
+++ database.py
@@ -3,40 +3,52 @@ import json
 import ast
 
 from collection import Collection
-from indexation import  Indexation
+from indexation import Indexation
 
 
 class Database:
     def __init__(self, path_to_database, current_collection: str):
         self.path_to_collection = os.path.join(path_to_database, current_collection)
-        os.makedirs(self.path_to_collection, exist_ok=True)
+        os.makedirs(self.path_to_collection,
+                    exist_ok=True)  # Создаст path_to_collection если его нет, либо проигнорирует, есть пусть уже создан
 
         self.collection = Collection(self.path_to_collection)
         self.indexation = Indexation(self.collection)
 
-    def insert(self, string: str):                                  # Пример: python cli_core.py db mydb/users insert "{'name': 'Иван', 'age': 18}"
+    def insert(self, string: str) -> str:
+        # Вставка json-объекта в выбранную базу данных.
+        # Пример 1: python cli_core.py db mydb/users insert "{'name': 'Иван', 'age': 18}"
+        # Пример 2: python cli_core.py --storage-path "E:\PyCharmProjects\kbdSQL\storage" db mydb/users insert "{'name': 'Иван', 'age': 18}"
         try:
             json_document = json.loads(string)
         except json.JSONDecodeError:
-            json_document = ast.literal_eval(string)
-        return self.collection.insert(json_document)
+            json_document = ast.literal_eval(string)  # type(json_document): dict
 
-    def index(self, field: str):                                    # Пример: python cli_core.py db mydb/users index age
-        return self.indexation.create_index(field)                  # Возвращает количество проиндексированных значений
+        try:
+            return self.collection.insert(json_document)  # Возвращает id json-документа
+        except Exception as error:
+            raise error
 
-    def search_by_condition(self, query: str):                      # Пример: python cli_core.py db mydb/users condition "{'age': {'@eq': 18}}"
+    def index(self, field: str) -> int:
+        # Индексация выбранного поля по всем json-объектам в выбранной базе данных.
+        # Пример 1: python cli_core.py db mydb/users index age
+        # Пример 2: python cli_core.py --storage-path "E:\PyCharmProjects\kbdSQL\storage" db mydb/users index age
+        try:
+            return self.indexation.create_index(field)  # Возвращает количество проиндексированных значений
+        except Exception as error:
+            raise error
+
+    def search_by_condition(self, query: str) -> list:
+        # Поиск json-документов по заданному условию в выбранной базе данных.
+        # Пример 1: python cli_core.py db mydb/users condition "{'age': {'@eq': 18}}"
+        # Пример 2: python cli_core.py --storage-path "E:\PyCharmProjects\kbdSQL\storage" db mydb/users condition "{'age': {'@eq': 18}}"
+
         try:
             query_dict = json.loads(query)
         except json.JSONDecodeError:
-            query_dict = ast.literal_eval(query)                    # Превращает "{'age': {'`$gt': 18}}" в словарь
-        return self.collection.search_by_condition(query_dict)      # Возвращает список "подходящих" json-документов
+            query_dict = ast.literal_eval(query)  # type(query_dict): dict
 
-
-    # (нужно дописать реализацию)
-    # (удаляет документ по id)
-    # def delete(self, doc_id: str):
-    #     pass
-
-    # (вывожит все коллекции)
-    # def list_collections(self):
-    #     pass
+        try:
+            return self.collection.search_by_condition(query_dict)  # Возвращает список "подходящих" json-документов
+        except Exception as error:
+            raise error
